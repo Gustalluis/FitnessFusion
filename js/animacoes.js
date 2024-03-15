ed51bf4914e2b054ed2c26ba2560922f8f557556
Gustalluis <150025091+Gustalluis@users.noreply.github.com>
2024-03-15 19:52:18 -0300

    mds tamu inso
    comite que fiz sexta no senac de noite

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,8 +1,29 @@
-$('.boxcomentarios').slick({
-  slidesToShow: 4, /*quantos quer visualizar*/
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000, /*velocidade*/ 
+$(document).ready(function(){
+  $('.comentario').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
 
 $('.bannerEvento').slick({
