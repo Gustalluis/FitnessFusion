8cdfc6361745a2a994fa3ec60dd5329e182d8456
gustalluis <gustavoluis2106@gmail.com>
2024-03-18 09:40:37 -0300

    comitaçoes finais

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,54 +1,54 @@
-$(document).ready(function(){
-  $('.comentario').slick({
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1
-        }
+$(".comentarios > div").slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
-$('.bannerEvento').slick({
-  slidesToShow: 1, /*quantos quer visualizar*/
+$(".bannerEvento").slick({
+  slidesToShow: 1 /*quantos quer visualizar*/,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000, /*velocidade*/ 
+  autoplaySpeed: 2000 /*velocidade*/,
 });
 
-/* WOW */ 
+/* WOW */
 new WOW().init();
 
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var imageContainers = document.querySelectorAll(".blog section > div");
 
-  imageContainers.forEach(function(container) {
-      container.addEventListener("mouseover", function() {
-          var overlay = this.querySelector(".blog section div div");
-          overlay.style.display = "block";
-      });
+  imageContainers.forEach(function (container) {
+    container.addEventListener("mouseover", function () {
+      var overlay = this.querySelector(".blog section div div");
+      overlay.style.display = "block";
+    });
 
-      container.addEventListener("mouseout", function() {
-          var overlay = this.querySelector(".blog section div div");
-          overlay.style.display = "none";
-      });
+    container.addEventListener("mouseout", function () {
+      var overlay = this.querySelector(".blog section div div");
+      overlay.style.display = "none";
+    });
   });
 });
